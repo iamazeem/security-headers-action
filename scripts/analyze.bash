aada3d5f29a03f00f1a1955264f7c28e946edb5b
Azeem Sajid <azeem.sajid@gmail.com>
2023-04-23 22:07:01 +0500

    Update action

--- scripts/analyze.bash
+++ scripts/analyze.bash
@@ -22,9 +22,7 @@ analyze() {
   # INPUT_MAX_RETRIES_ON_API_ERROR
 
   local API_URL="https://api.securityheaders.com/?q=$INPUT_DOMAIN_OR_URL&followRedirects=$INPUT_FOLLOW_REDIRECTS&hide=$INPUT_HIDE_RESULTS_ON_HOMEPAGE"
-
   echo "Making API request [$API_URL]"
-
   local API_RESPONSE="$(curl -s -m "$INPUT_API_TIMEOUT_IN_SECONDS" -H "x-api-key: $INPUT_API_KEY" "$API_URL" 2>&1)"
   if [[ -z $API_RESPONSE ]]; then
     echo "Empty response!"
@@ -35,32 +33,34 @@ analyze() {
     exit 1
   fi
 
-  echo "Checking response for status"
+  echo "Checking status"
   local STATUS="$(jq -r '.status | ascii_downcase' <<<"$API_RESPONSE")"
   if [[ $STATUS != "good" ]]; then
     echo "Invalid response status received! [$STATUS]"
     jq -r '.' <<<"$API_RESPONSE"
     exit 1
   else
-    echo "Status: [$STATUS]"
+    echo "STATUS: [$STATUS]"
   fi
 
   if [[ -n $INPUT_EXPECTED_GRADE ]]; then
-    echo "Checking response for expected grade [$INPUT_EXPECTED_GRADE]"
-    local GRADE="$(jq -rc '.summary.grade | ascii_upcase' <<<"$API_RESPONSE")"
+    echo "Checking expected grade [$INPUT_EXPECTED_GRADE]"
+    local GRADE="$(jq -r '.summary.grade | ascii_upcase' <<<"$API_RESPONSE")"
+    echo "Expected: [$INPUT_EXPECTED_GRADE], Found: [$GRADE]"
     if [[ $INPUT_EXPECTED_GRADE != "$GRADE" ]]; then
       echo "Unexpected grade found!"
-      echo "Expected: [$INPUT_EXPECTED_GRADE], Found: [$GRADE]"
       exit 1
     else
       echo "Expected grade found! [$INPUT_EXPECTED_GRADE]"
     fi
   fi
 
+  echo "Setting output parameters"
+
   {
     echo "$OUTPUT_RESULTS_AS_JSON=$(jq -rc '.' <<<"$API_RESPONSE")"
     echo "$OUTPUT_SUMMARY_AS_JSON=$(jq -rc '.summary' <<<"$API_RESPONSE")"
-    echo "$OUTPUT_GRADE=$(jq -rc '.summary.grade | ascii_upcase' <<<"$API_RESPONSE")"
+    echo "$OUTPUT_GRADE=$(jq -r '.summary.grade | ascii_upcase' <<<"$API_RESPONSE")"
   } | tee -a "$GITHUB_OUTPUT"
 
   echo "::endgroup::"
